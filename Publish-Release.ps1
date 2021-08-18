9a69f126ab08a03862fefa0832441fa42c26cf08
J Burnett <jburnett@users.noreply.github.com>
2021-08-18 17:09:58 -0400

    Master -> dev merge (#3)
    * Adds net5.0 support (#2)
    
    * Created Dependabot config
    
    * Created build workflow
    
    * Constrained to specific test projects
    
    * Break tests into individual steps
    
    * Include .NET 2.1 since LTS & targeted in csprojs
    
    * Added netstd 2.1 to common PROD properties
    
    * Updated copyright to 2021
    
    * Upgrade .NET SDK versions
    
    * Add net5.0 support; bump minor version
    
    * Add .NET 5.0 to Actions
    
    * Update Publish to match AMT's

--- Publish-Release.ps1
+++ Publish-Release.ps1
@@ -2,6 +2,12 @@ param (
     [Parameter(Mandatory=$true)][string]$apiKey
 )
 
+$branch = invoke-expression "git branch --show-current"
+
+if ($branch -ne 'master') {
+    Write-Error "Publishing only allowed from master branch"
+    exit
+}
 
 $projPaths = @(
     "J3DI.Domain",
@@ -9,8 +15,14 @@ $projPaths = @(
     "J3DI.Infrastructure.RepositoryFx"
 )
 
-
 # Ensure everything is built
+Write-Verbose "Pack projects"
+$projPaths | %{
+    write-host ""   # separator
+    dotnet pack -c Release $_
+}
+
+# Publish to NuGet
 Write-Verbose "Publish projects"
 $projPaths | %{
     write-host ""   # separator
